11f74999c152521efa023c4ba42e9f0c5908cc82
gcorp2026 <spike.morel@gmail.com>
2026-04-02 11:28:19 +0300

    Add Config, Content, Source, uproject

---
+++ Source/atomichearts/atomichearts.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class atomichearts : ModuleRules
+{
+	public atomichearts(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"atomichearts",
+			"atomichearts/Variant_Platforming",
+			"atomichearts/Variant_Platforming/Animation",
+			"atomichearts/Variant_Combat",
+			"atomichearts/Variant_Combat/AI",
+			"atomichearts/Variant_Combat/Animation",
+			"atomichearts/Variant_Combat/Gameplay",
+			"atomichearts/Variant_Combat/Interfaces",
+			"atomichearts/Variant_Combat/UI",
+			"atomichearts/Variant_SideScrolling",
+			"atomichearts/Variant_SideScrolling/AI",
+			"atomichearts/Variant_SideScrolling/Gameplay",
+			"atomichearts/Variant_SideScrolling/Interfaces",
+			"atomichearts/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/atomichearts/atomichearts.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "atomichearts.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, atomichearts, "atomichearts" );
+
+DEFINE_LOG_CATEGORY(Logatomichearts)
